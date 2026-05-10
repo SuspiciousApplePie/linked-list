059d8a47f49d8efb86165b39d84ff7006a0ab28b
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-10 20:04:48 +0800

    Add main js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import { linkedList } from "./linkedList.js";
+
+const newLinkedList = linkedList();
+
+newLinkedList.append("Bron");
+newLinkedList.append("Bron");
+newLinkedList.append("Bron");
+newLinkedList.append("Bron");
+console.log(newLinkedList.append("Kobe"));
